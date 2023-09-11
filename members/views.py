d4e34db71ca6d41e9913bdd39e427ca23ab3cd0e
furucrm-thienpham <thien.pham@furucrm.com>
2023-09-11 15:33:27 +0700

    learning django

---
+++ members/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+# Import model 
+from .models import Member
+
+#index 
+def index(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+#get all member templates
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+#get detail of members
+def details(request, id): 
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context, request))
+
+def testing(request):
+    members = Member.objects.all().values()
+    template = loader.get_template('template.html')
+    context = {
+        'firstname': 'Linus',
+        'mymembers': members,
+        'fruits': ['Apple', 'Banana', 'Cherry', 'Orange']
+    }
+    return HttpResponse(template.render(context, request))
